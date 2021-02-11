194eb74bfacd8b9a7e8abdeb5edf6b0876bb0b76
Turar Nurdaulet <dicenameismy>
2021-02-11 13:00:37 +0600

    database setup

---
+++ app/database_create.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+connection = sqlite3.connect("UserData.db")
+cursor = connection.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS questions
+    (_id INTEGER PRIMARY KEY,
+     author_id VARCHAR(9),
+     question VARCHAR(256),
+     best_answer_id INTEGER)
+""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS answers
+    (_id INTEGER PRIMARY KEY,
+    author VARCHAR(9),
+    question_id INTEGER,
+    answer VARCHAR(256))
+""")    
+
+
+connection.commit()
+connection.close()
