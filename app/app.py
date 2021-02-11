0dc4fb3b6f06a9df81f62f2329c8aca393f5030b
Turar Nurdaulet <dicenameismy>
2021-02-11 18:57:14 +0600

    required constants

--- app/app.py
+++ app/app.py
@@ -10,3 +10,13 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.utils.helper import Helper, HelperMode, ListItem
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
+
+main_text = {
+    'ask': 'Задать вопрос', 'req': 'Запросить вопрос', 
+    'my_q': 'Мои вопросы', 'back': 'Вернуться назад', 
+    'cont': 'Продолжить',
+    }
+
+manage_text = {'add': 'Добавить вопрос', 'del': 'Удалить вопрос'}
+
+temporary = {}
